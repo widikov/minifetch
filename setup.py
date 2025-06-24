ec18c39fb28d2cd5340428deb5a153fca8852025
wdkq <110486890+widikov@users.noreply.github.com>
2025-06-24 22:32:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="minifetch-tool",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     include_package_data=True,
     package_data={

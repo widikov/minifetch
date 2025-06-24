20d9a16fb9ad2ebf15e2de1473b968c55d61170a
wdkq <110486890+widikov@users.noreply.github.com>
2025-06-24 06:08:14 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="minifetch",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'colorama',
+        'psutil',
+        'distro; platform_system=="Linux"',
+    ],
+    entry_points={
+        'console_scripts': [
+            'minifetch=minifetch:entry_point',
+        ],
+    },
+    author="wdkq",
+    author_email="koc2b93o0@mozmail.com",
+    description="A mini system information fetch tool.",
+    license="MIT",
+    keywords="system info fetch",
+    url="https://github.com/widikov/minifetch",
+)

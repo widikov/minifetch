904ee1f8484bb92cf055c6a896842cd6fec7df52
wdkq <110486890+widikov@users.noreply.github.com>
2025-06-24 07:05:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="minifetch-tool",
-    version="1.1.1",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     package_data={
